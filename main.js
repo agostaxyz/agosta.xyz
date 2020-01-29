bd87754c8ee4b2a5de7a3c25ffae3a6999cee298
Luca Agosta <me@agosta.dev>
2020-01-29 00:48:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("ERROR: Power user not expected");
+console.log("TYPE: konami");
+
+function colorFunc () {    
+    const mouseTarget = document.getElementById('aqua');
+    
+    document.getElementById("blue").style.borderBottom = "4px solid lightblue";
+    document.getElementById("green").style.borderBottom = "4px solid lightgreen";
+
+}
