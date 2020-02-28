02a67c16d2d6d460ef345e5e69fcc18f675750b1
GeladeiraTsunami <me@agosta.dev>
2020-02-27 23:01:56 -0300

    Update

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 console.log("ERROR: Power user not expected");
 console.log("TYPE: konami");
 
-function colorFuncQ () {    
-
+function colorFunc () {    
+    document.getElementById("pink").style.borderBottom = "4px solid pink";
     document.getElementById("lightblue").style.borderBottom = "3px solid lightblue";
     document.getElementById("lightgreen").style.borderBottom = "3px solid lightgreen";
-    document.getElementById("pdf").style.background = "red";
-    document.getElementById("pdf").style.border = "4px solid red";
-    
-
-}
-
-function colorFuncH () {
-    document.getElementById("pink").style.borderBottom = "4px solid pink";
 }
