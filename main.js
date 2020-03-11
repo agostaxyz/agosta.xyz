08f128f5c06d8dd09be207a1fdbccf9e99e673e2
Luca Agosta <me@agosta.dev>
2020-03-11 01:45:49 -0300

    Despacito

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-console.log("ERROR: Power user not expected");
-console.log("TYPE: konami");
+console.log('ERROR: Killer robots desengaged, type "konami" to see log');
+
+function mainFunc () {
+    
+
+}
+
 
 function colorFunc () {    
     document.getElementById("pink").style.borderBottom = "4px solid pink";
     document.getElementById("lightblue").style.borderBottom = "3px solid lightblue";
     document.getElementById("lightgreen").style.borderBottom = "3px solid lightgreen";
 }
+
+function despacito () {
+    /*Set active project tab to "ALL"*/
+
+
+}   
