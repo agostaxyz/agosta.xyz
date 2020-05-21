d3dcf23e0f1b20610df954efce981b73bd93ee95
Luca Agosta <me@agosta.dev>
2020-05-21 20:14:08 -0300

    Update deployment.sh

--- dotfiles/deployment.sh
+++ dotfiles/deployment.sh
@@ -1,6 +1,4 @@
-sudo xbps-install -Suy git
 mkdir git_workspace
-
+sudo xbps-install -Suy git
 git clone https://github.com/Coldsaga/dotfiles.git git_workspace/dotfiles
-
 sh git_workspace/dotfiles/deployment.sh
