5c5d237bf3ca58d72c85a2e66e61af67424e74ba
Coldsaga <me@agosta.dev>
2020-08-28 13:27:11 -0300

    added page loading and 404 page

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-console.log('Hello suspicious dev!');
+function loadClear() {
+    document.getElementById("loader-wrapper").style.display= "none";
+};
+
 console.log('Awaiting Konami code.');
 
 function colorFunc() {
     document.getElementById("pink").style.borderBottom = "4px solid pink";
     document.getElementById("lightblue").style.borderBottom = "3px solid lightblue";
     document.getElementById("lightgreen").style.borderBottom = "3px solid lightgreen";
-}
+};
 
 function despacito() {
     /*Set active project tab to "ALL"*/
 
 
-}
+};
